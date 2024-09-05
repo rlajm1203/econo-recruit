2bee43f87930aa615e6149755e8d58209ed3e275
KimJongMin <whdals011203@naver.com>
2024-09-06 01:22:29 +0900

    feat: 칸반 조회시 파라미터에 year 추가

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/card/usecase/CardLoadUseCase.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/card/usecase/CardLoadUseCase.java
@@ -4,13 +4,15 @@ import com.econovation.recruitcommon.annotation.UseCase;
 import com.econovation.recruitdomain.domains.card.domain.Card;
 import com.econovation.recruitdomain.domains.card.dto.BoardCardResponseDto;
 import com.econovation.recruitdomain.domains.card.dto.CardResponseDto;
+import org.springframework.http.HttpStatus;
+
 import java.util.List;
 
 @UseCase
 public interface CardLoadUseCase {
     List<Card> findAll();
 
-    List<BoardCardResponseDto> getByNavigationId(Integer navigationId);
+    List<BoardCardResponseDto> getByNavigationIdAndYear(Integer navigationId, Integer year);
 
     CardResponseDto findCardById(Long cardId);
 }
